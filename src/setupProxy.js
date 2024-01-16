91c70f99bca23bb525b6d8205b24983d302898ec
Khoa Tran <77377243+khoatran73@users.noreply.github.com>
2024-01-16 21:12:40 +0700

    chore: log environment variables

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
+    console.log('host: ', process.env.REACT_APP_PROXY_API_HOST)
+    console.log('api url: ', process.env.REACT_APP_API_URL)
+    console.log('node: ', process.env.NODE_ENV)
+
     app.use(
         process.env.REACT_APP_API_URL ?? '/api',
         createProxyMiddleware({
