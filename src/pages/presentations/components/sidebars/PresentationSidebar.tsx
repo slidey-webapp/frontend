6e42a11d001c4571d8f596a20e14f97e8f0bce40
Khoa Tran <77377243+khoatran73@users.noreply.github.com>
2024-01-28 21:18:28 +0700

    feat: duplicate and remove slide

--- src/pages/presentations/components/sidebars/PresentationSidebar.tsx
+++ src/pages/presentations/components/sidebars/PresentationSidebar.tsx
@@ -1,9 +1,15 @@
+import { Divider } from '@mui/material';
 import clsx from 'clsx';
 import _ from 'lodash';
 import React from 'react';
 import { DragDropContext, Draggable, Droppable, OnDragEndResponder } from 'react-beautiful-dnd';
+import { ContextMenu, ContextMenuTrigger, MenuItem } from 'react-contextmenu';
+import BaseIcon from '~/components/icons/BaseIcon';
+import { requestApi } from '~/libs/axios';
+import NotifyUtil from '~/utils/NotifyUtil';
 import { usePresentationContext } from '../../PresentationDetailPage';
-import { SlideDto } from '../../types/slide';
+import { PRESENTATION_CREATE_SLIDE_API } from '../../api/presentation.api';
+import { SlideDto, SlideType } from '../../types/slide';
 import NewSlidePattern from './NewSlidePattern';
 import OverviewHeadingSlide from './OverviewHeadingSlide';
 import OverviewMultipleChoiceSlide from './OverviewMultipleChoiceSlide';
@@ -20,7 +26,8 @@ const reorder = (list: SlideDto[], startIndex: number, endIndex: number) => {
 };
 
 const PresentationSidebar: React.FC<Props> = () => {
-    const { presentation, currentSlideId, slides, onUpdatePresentation, setCurrentSlideId } = usePresentationContext();
+    const { presentation, currentSlideId, presentationID, slides, onUpdatePresentation, setCurrentSlideId } =
+        usePresentationContext();
 
     const onDragEnd: OnDragEndResponder = result => {
         if (!result.destination || result.destination?.index === result.source.index) return;
@@ -47,6 +54,64 @@ const PresentationSidebar: React.FC<Props> = () => {
         }
     };
 
+    const handleDeleteSlide = async (slide: SlideDto) => {
+        const confirm = await NotifyUtil.confirmDialog(
+            'Bạn có chắc muốn xóa?',
+            'Thao tác này sẽ xóa vĩnh viễn slide và mọi dữ liệu phản hồi liên quan sẽ bị mất',
+        );
+
+        if (confirm.isDismissed) return;
+
+        const newSlides = _.cloneDeep(slides);
+        const slideIndex = newSlides.findIndex(x => x.slideID === slide.slideID);
+
+        newSlides.splice(slideIndex, 1);
+
+        await onUpdatePresentation({
+            slides: newSlides,
+        });
+
+        if (slide.slideID !== currentSlideId) return;
+
+        if (slideIndex === 0) {
+            setCurrentSlideId(newSlides[0]?.slideID);
+        } else {
+            setCurrentSlideId(newSlides[slideIndex - 1]?.slideID);
+        }
+    };
+
+    const handleDuplicateSlide = async (slide: SlideDto) => {
+        const newSlide = await createSlide(slide.type);
+        if (!newSlide) return;
+
+        const newSlides = _.cloneDeep(slides);
+        const slideCloned = _.cloneDeep(slide);
+        // @ts-ignore
+        delete slideCloned.slideID;
+
+        newSlides.push({
+            ...newSlide,
+            heading: slideCloned.heading,
+            subHeading: slideCloned.subHeading,
+            paragraph: slideCloned.paragraph,
+            question: slideCloned.question,
+            options: slideCloned.options,
+        });
+
+        onUpdatePresentation({
+            slides: newSlides,
+        });
+    };
+
+    const createSlide = async (type: SlideType) => {
+        const response = await requestApi<SlideDto>('post', PRESENTATION_CREATE_SLIDE_API, {
+            presentationID,
+            type,
+        });
+
+        if (response.status === 200) return response.data.result;
+    };
+
     return (
         <div className="w-44 h-full overflow-hidden flex flex-col">
             <div className="w-full h-14 flex items-center px-4">
@@ -61,39 +126,59 @@ const PresentationSidebar: React.FC<Props> = () => {
                                     {slides.map((slide, index) => {
                                         const active = slide.slideID === currentSlideId;
                                         return (
-                                            <Draggable
-                                                key={slide.slideID}
-                                                draggableId={slide.slideID.toString()}
-                                                index={index}
-                                            >
-                                                {(provided, snapshot) => {
-                                                    return (
-                                                        <div
-                                                            className="w-full h-18 flex items-center mb-4"
-                                                            key={index}
-                                                            ref={provided.innerRef}
-                                                            {...provided.draggableProps}
-                                                            {...provided.dragHandleProps}
-                                                        >
-                                                            <div className="w-8 h-full flex items-center justify-center">
-                                                                {!snapshot.isDragging && <span>{index + 1}</span>}
-                                                            </div>
-                                                            <div
-                                                                className={clsx(
-                                                                    'flex-1 h-full border-2 p-2 bg-white flex items-center justify-center border-neutral-100 rounded ',
-                                                                    'cursor-pointer transition-all duration-200 ease-in-out hover:border-neutral-300',
-                                                                    {
-                                                                        '!border-indigo-main': active,
-                                                                    },
-                                                                )}
-                                                                onClick={() => setCurrentSlideId(slide.slideID)}
-                                                            >
-                                                                {renderOverviewSlide(slide)}
-                                                            </div>
-                                                        </div>
-                                                    );
-                                                }}
-                                            </Draggable>
+                                            <>
+                                                {/* @ts-ignore */}
+                                                <ContextMenuTrigger
+                                                    id={'menu-context-' + slide.slideID}
+                                                    key={slide.slideID}
+                                                >
+                                                    <Draggable draggableId={slide.slideID.toString()} index={index}>
+                                                        {(provided, snapshot) => {
+                                                            return (
+                                                                <div
+                                                                    className="w-full h-18 flex items-center mb-4"
+                                                                    key={index}
+                                                                    ref={provided.innerRef}
+                                                                    {...provided.draggableProps}
+                                                                    {...provided.dragHandleProps}
+                                                                >
+                                                                    <div className="w-8 h-full flex items-center justify-center">
+                                                                        {!snapshot.isDragging && (
+                                                                            <span>{index + 1}</span>
+                                                                        )}
+                                                                    </div>
+                                                                    <div
+                                                                        className={clsx(
+                                                                            'flex-1 h-full border-2 p-2 bg-white flex items-center justify-center border-neutral-100 rounded ',
+                                                                            'cursor-pointer transition-all duration-200 ease-in-out hover:border-neutral-300',
+                                                                            {
+                                                                                '!border-indigo-main': active,
+                                                                            },
+                                                                        )}
+                                                                        onClick={() => setCurrentSlideId(slide.slideID)}
+                                                                    >
+                                                                        {renderOverviewSlide(slide)}
+                                                                    </div>
+                                                                </div>
+                                                            );
+                                                        }}
+                                                    </Draggable>
+                                                </ContextMenuTrigger>
+                                                {/* @ts-ignore */}
+                                                <ContextMenu id={'menu-context-' + slide.slideID}>
+                                                    {/* @ts-ignore */}
+                                                    <MenuItem onClick={() => handleDuplicateSlide(slide)}>
+                                                        <BaseIcon type="duplicate" />
+                                                        <div className="ml-3">Nhân đôi slide</div>
+                                                    </MenuItem>
+                                                    <Divider style={{ margin: '4px 0' }} />
+                                                    {/* @ts-ignore */}
+                                                    <MenuItem onClick={() => handleDeleteSlide(slide)}>
+                                                        <BaseIcon type="delete-outlined" />
+                                                        <div className="ml-3">Xóa slide</div>
+                                                    </MenuItem>
+                                                </ContextMenu>
+                                            </>
                                         );
                                     })}
                                     {droppableProvided.placeholder}
