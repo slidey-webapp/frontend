8b72bb07ce371dfaf8b2c790c496f096c5e3e046
Khoa Tran <77377243+khoatran73@users.noreply.github.com>
2024-03-04 22:27:43 +0700

    feat: create presentation from template

--- src/pages/templates/TemplatePage.tsx
+++ src/pages/templates/TemplatePage.tsx
@@ -1,153 +1,26 @@
-import clsx from 'clsx';
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { BreadcrumbRef } from '~/components/bread-crumb/BreadCrumb';
 import { ButtonBase } from '~/components/buttons/ButtonBase';
 import { AppContainer } from '~/components/layouts/AppContainer';
-import Loading from '~/components/loadings/Loading';
-import { ComboOptionConstant } from '~/configs/constants';
-import { PaginatedList, requestApi } from '~/libs/axios';
-import { PresentationDto } from '../presentations/types/presentation';
-import { SlideDto, SlideType } from '../presentations/types/slide';
-import { TEMPLATE_INDEX_API } from './api/template.api';
-import TemplateItem from './components/TemplateItem';
 import TemplateList from './components/TemplateList';
-import Overlay, { OverlayRef } from '~/components/loadings/Overlay';
-import { PRESENTATION_CREATE_API } from '../presentations/api/presentation.api';
-import NotifyUtil from '~/utils/NotifyUtil';
 
 interface Props {}
 
 const TemplatePage: React.FC<Props> = () => {
     const navigate = useNavigate();
-    const [templates, setTemplates] = useState<PresentationDto[]>([]);
-    const [isLoading, setIsLoading] = useState<boolean>(true);
-    const [chosen, setChosen] = useState<PresentationDto | null>(null);
-
-    const overlayRef = useRef<OverlayRef>(null);
-    const breadcrumbRef = useRef<BreadcrumbRef>(null);
-
-    useEffect(() => {
-        const fetchTemplates = () => {
-            requestApi<PaginatedList<PresentationDto>>('get', TEMPLATE_INDEX_API, null, {
-                params: {
-                    offset: 0,
-                    limit: 10,
-                },
-            })
-                .then(res => {
-                    if (res.status === 200) {
-                        setTemplates(res.data.result?.items || []);
-                    }
-                })
-                .finally(() => setIsLoading(false));
-        };
-        fetchTemplates();
-    }, []);
 
     const handleCreate = () => {
         navigate('/template/create');
     };
 
-    const handleDetail = (data: PresentationDto) => {
-        setChosen(data);
-    };
-
-    const handleCreatePresent = async (data: PresentationDto) => {
-        overlayRef.current?.open();
-        const response = await requestApi<{
-            presentation: PresentationDto;
-            slides: SlideDto[];
-        }>('post', PRESENTATION_CREATE_API, {
-            name: data.name,
-            templateID: data.presentationID,
-        });
-        overlayRef.current?.close();
-
-        if (response.status !== 200) {
-            response.data.message && NotifyUtil.error(response.data.message);
-            return;
-        }
-
-        response.data.result?.presentation?.presentationID &&
-            navigate('/presentation/edit/' + response.data.result?.presentation?.presentationID);
-    };
-
-    const renderBody = () => {
-        if (isLoading) return <Loading />;
-        if (chosen) {
-            const slideTypes = new Set<SlideType>();
-            chosen.slides?.forEach(slide => slideTypes.add(slide.type));
-
-            return (
-                <div className="flex gap-x-6 w-full">
-                    <div className="flex-1 sticky h-full top-0 bg-white">
-                        <div className="w-full">
-                            <div className="text-xl font-semibold mb-3">{chosen.name}</div>
-                            <div className="text-xs font-semibold mb-2">Bao gồm các loại slide</div>
-                            <div className="flex flex-wrap gap-2">
-                                {[...slideTypes].map(type => {
-                                    const label = ComboOptionConstant.SLIDE_TYPE.find(x => x.value === type)?.label;
-                                    if (!label) return null;
-
-                                    return (
-                                        <div
-                                            key={type}
-                                            className="px-2 h-6 flex items-center justify-center rounded-full border border-neutral-300 text-neutral-500 text-xs"
-                                        >
-                                            {label}
-                                        </div>
-                                    );
-                                })}
-                            </div>
-                            <div className="mt-7 w-full">
-                                <div
-                                    className={clsx(
-                                        'h-12 px-6 w-fit rounded-full flex items-center justify-center bg-neutral-700 text-white font-semibold',
-                                        'cursor-pointer transition-all duration-300 ease-in-out hover:bg-black',
-                                    )}
-                                    onClick={() => handleCreatePresent(chosen)}
-                                >
-                                    Dùng mẫu
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex-2">
-                        <div className="w-full grid grid-cols-2 gap-4 select-none">
-                            {chosen.slides?.map((slide, idx) => {
-                                return (
-                                    <div key={slide.slideID} className="col-span-1">
-                                        <TemplateItem slide={slide} index={idx + 1} />
-                                    </div>
-                                );
-                            })}
-                        </div>
-                    </div>
-                </div>
-            );
-        }
-
-        return <TemplateList templates={templates} onDetail={handleDetail} onCreatePresent={handleCreatePresent} />;
-    };
-
     return (
-        <AppContainer className="flex flex-col overflow-hidden" breadcrumbRef={breadcrumbRef}>
+        <AppContainer className="flex flex-col overflow-hidden">
             <div className="flex items-center justify-end gap-x-2.5 w-full h-fit min-h-[40px] mb-7">
-                {chosen !== null && (
-                    <ButtonBase
-                        onClick={() => {
-                            setChosen(null);
-                        }}
-                        color={'secondary'}
-                        title="Trở về"
-                        startIcon={'arrow-back'}
-                    />
-                )}
                 <ButtonBase onClick={handleCreate} color={'success'} title="Tạo mới" startIcon={'add'} />
             </div>
-            <div className="base-grid p-7 flex-1 relative overflow-x-hidden overflow-y-auto">{renderBody()}</div>
-            <Overlay ref={overlayRef} />
+            <div className="base-grid p-7 flex-1 relative overflow-x-hidden overflow-y-auto">
+                <TemplateList />
+            </div>
         </AppContainer>
     );
 };
--- src/pages/templates/components/TemplateList.tsx
+++ src/pages/templates/components/TemplateList.tsx
@@ -1,76 +1,217 @@
 import { Grid, Stack } from '@mui/material';
 import clsx from 'clsx';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import BaseIcon from '~/components/icons/BaseIcon';
+import Loading from '~/components/loadings/Loading';
+import { ComboOptionConstant } from '~/configs/constants';
+import { PaginatedList, requestApi } from '~/libs/axios';
+import { PRESENTATION_CREATE_API } from '~/pages/presentations/api/presentation.api';
 import { PresentationDto } from '~/pages/presentations/types/presentation';
+import { SlideDto, SlideType } from '~/pages/presentations/types/slide';
+import NotifyUtil from '~/utils/NotifyUtil';
+import { TEMPLATE_INDEX_API } from '../api/template.api';
 import TemplateItem from './TemplateItem';
+import Overlay, { OverlayRef } from '~/components/loadings/Overlay';
 
 export interface Props {
-    templates: PresentationDto[];
-    onDetail: (data: PresentationDto) => void;
-    onCreatePresent: (data: PresentationDto) => void;
+    renderAddonBeforeItem?: () => JSX.Element;
 }
 
-const TemplateList: React.FC<Props> = ({ templates, onDetail, onCreatePresent }) => {
-    return (
-        <Stack useFlexGap gap={2}>
-            <Grid
-                container
-                spacing={3}
-                sx={{
-                    position: 'relative',
-                }}
-            >
-                {templates.map(item => {
-                    return (
-                        <Grid key={item.presentationID} item xs={12} sm={6} md={4}>
-                            <Stack direction="column">
+const TemplateList: React.FC<Props> = ({ renderAddonBeforeItem }) => {
+    const navigate = useNavigate();
+
+    const [templates, setTemplates] = useState<PresentationDto[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [chosen, setChosen] = useState<PresentationDto | null>(null);
+
+    const overlayRef = useRef<OverlayRef>(null);
+
+    useEffect(() => {
+        const fetchTemplates = () => {
+            requestApi<PaginatedList<PresentationDto>>('get', TEMPLATE_INDEX_API, null, {
+                params: {
+                    offset: 0,
+                    limit: 10,
+                },
+            })
+                .then(res => {
+                    if (res.status === 200) {
+                        setTemplates(res.data.result?.items || []);
+                    }
+                })
+                .finally(() => setIsLoading(false));
+        };
+        fetchTemplates();
+    }, []);
+
+    const handleDetail = (data: PresentationDto) => {
+        setChosen(data);
+    };
+
+    const handleCreatePresent = async (data: PresentationDto) => {
+        overlayRef.current?.open();
+        const response = await requestApi<{
+            presentation: PresentationDto;
+            slides: SlideDto[];
+        }>('post', PRESENTATION_CREATE_API, {
+            name: data.name,
+            templateID: data.presentationID,
+        });
+        overlayRef.current?.close();
+
+        if (response.status !== 200) {
+            response.data.message && NotifyUtil.error(response.data.message);
+            return;
+        }
+
+        response.data.result?.presentation?.presentationID &&
+            navigate('/presentation/edit/' + response.data.result?.presentation?.presentationID);
+    };
+
+    const renderBody = () => {
+        if (isLoading)
+            return (
+                <div
+                    className="relative "
+                    style={{
+                        height: 400,
+                        width: '100%',
+                    }}
+                >
+                    <Loading />
+                </div>
+            );
+        if (chosen) {
+            const slideTypes = new Set<SlideType>();
+            chosen.slides?.forEach(slide => slideTypes.add(slide.type));
+
+            return (
+                <div className="flex gap-x-6 w-full">
+                    <div className="flex-1 sticky h-full top-0 bg-white">
+                        <div className="w-full">
+                            <div
+                                className={clsx(
+                                    'text-xs font-semibold mb-7 flex items-center cursor-pointer text-black',
+                                    'transition-all ease-in-out duration-200 hover:text-neutral-500',
+                                )}
+                                onClick={() => setChosen(null)}
+                            >
+                                <BaseIcon type="arrow-back" size={14} /> <span className="ml-1">Trở về</span>
+                            </div>
+                            <div className="text-xl font-semibold mb-3">{chosen.name}</div>
+                            <div className="text-xs font-semibold mb-2">Bao gồm các loại slide</div>
+                            <div className="flex flex-wrap gap-2">
+                                {[...slideTypes].map(type => {
+                                    const label = ComboOptionConstant.SLIDE_TYPE.find(x => x.value === type)?.label;
+                                    if (!label) return null;
+
+                                    return (
+                                        <div
+                                            key={type}
+                                            className="px-2 h-6 flex items-center justify-center rounded-full border border-neutral-300 text-neutral-500 text-xs"
+                                        >
+                                            {label}
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                            <div className="mt-7 w-full">
                                 <div
-                                    className="cursor-pointer select-none relative group"
-                                    onClick={() => onDetail(item)}
+                                    className={clsx(
+                                        'h-12 px-6 w-fit rounded-full flex items-center justify-center bg-neutral-700 text-white font-semibold',
+                                        'cursor-pointer transition-all duration-300 ease-in-out hover:bg-black',
+                                    )}
+                                    onClick={() => handleCreatePresent(chosen)}
                                 >
-                                    <TemplateItem slide={item.slides?.[0]} />
-                                    <div className="mt-2">
-                                        <div className="text-neutral-800 text-lg line-clamp-1">{item.name}</div>
-                                        <div className="text-sm text-neutral-500 line-clamp-1">
-                                            {item.slides?.length || 0} slides
-                                        </div>
-                                    </div>
-                                    <div
-                                        className={clsx('absolute  hidden group-hover:!flex gap-x-2')}
-                                        style={{
-                                            right: 10,
-                                            bottom: 66,
-                                        }}
-                                    >
-                                        <div
-                                            className={clsx(
-                                                'rounded-full h-8 text-13px border-2 border-indigo-main bg-indigo-main',
-                                                'flex items-center justify-center transition-all duration-200 ease-in-out',
-                                                'text-white hover:bg-white hover:text-indigo-main px-3',
-                                            )}
-                                            onClick={() => onDetail(item)}
-                                        >
-                                            Xem trước
-                                        </div>
-                                        <div
-                                            className={clsx(
-                                                'rounded-full h-8 text-xs border-2 border-indigo-main bg-indigo-main',
-                                                'flex items-center justify-center transition-all duration-200 ease-in-out',
-                                                'text-white hover:bg-white hover:text-indigo-main px-3',
-                                            )}
-                                            onClick={() => onCreatePresent(item)}
-                                        >
-                                            <BaseIcon type="add" size={18} />
-                                        </div>
-                                    </div>
+                                    Dùng mẫu
                                 </div>
-                            </Stack>
-                        </Grid>
-                    );
-                })}
-            </Grid>
-        </Stack>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex-2">
+                        <div className="w-full grid grid-cols-2 gap-4 select-none">
+                            {chosen.slides?.map((slide, idx) => {
+                                return (
+                                    <div key={slide.slideID} className="col-span-1">
+                                        <TemplateItem slide={slide} index={idx + 1} />
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+
+        return (
+            <Stack useFlexGap gap={2}>
+                <Grid
+                    container
+                    spacing={3}
+                    sx={{
+                        position: 'relative',
+                    }}
+                >
+                    {renderAddonBeforeItem?.()}
+                    {templates.map(item => {
+                        return (
+                            <Grid key={item.presentationID} item xs={12} sm={6} md={4}>
+                                <Stack direction="column">
+                                    <div
+                                        className="cursor-pointer select-none relative group"
+                                        onClick={() => handleDetail(item)}
+                                    >
+                                        <TemplateItem slide={item.slides?.[0]} />
+                                        <div className="mt-2">
+                                            <div className="text-neutral-800 text-lg line-clamp-1">{item.name}</div>
+                                            <div className="text-sm text-neutral-500 line-clamp-1">
+                                                {item.slides?.length || 0} slides
+                                            </div>
+                                        </div>
+                                        <div
+                                            className={clsx('absolute  hidden group-hover:!flex gap-x-2')}
+                                            style={{
+                                                right: 10,
+                                                bottom: 66,
+                                            }}
+                                        >
+                                            <div
+                                                className={clsx(
+                                                    'rounded-full h-8 text-13px border-2 border-indigo-main bg-indigo-main',
+                                                    'flex items-center justify-center transition-all duration-200 ease-in-out',
+                                                    'text-white hover:bg-white hover:text-indigo-main px-3',
+                                                )}
+                                                onClick={() => handleDetail(item)}
+                                            >
+                                                Xem trước
+                                            </div>
+                                            <div
+                                                className={clsx(
+                                                    'rounded-full h-8 text-xs border-2 border-indigo-main bg-indigo-main',
+                                                    'flex items-center justify-center transition-all duration-200 ease-in-out',
+                                                    'text-white hover:bg-white hover:text-indigo-main px-3',
+                                                )}
+                                                onClick={() => handleCreatePresent(item)}
+                                            >
+                                                <BaseIcon type="add" size={18} />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Stack>
+                            </Grid>
+                        );
+                    })}
+                </Grid>
+            </Stack>
+        );
+    };
+
+    return (
+        <>
+            {renderBody()}
+            <Overlay ref={overlayRef} />
+        </>
     );
 };
 
